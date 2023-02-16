77bcdb6228911c8ddc07013738fc017d93dacdf2
Prisca C <prisca.castanier@gmail.com>
2023-02-16 18:45:23 +0100

    fix: duplicate id leading to issue when removing product from cart

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,13 +1,24 @@
-import { item } from '~types/item';
+import { product } from '~types/product';
 import Styles from './ProductItem.module.css';
 import { useDispatch} from 'react-redux';
 import { addToCart } from 'src/features/cart/cartSlice';
+import { useSelector } from 'react-redux';
 
-const ProductItem = (props: item) => {
+const ProductItem = (props: product) => {
   const dispatch = useDispatch();
+  const cart = useSelector((state: any) => state.cart);
+  const lastItem = cart.items[cart.items.length - 1];
+  const id = lastItem ? lastItem.id + 1 : 1;
+  const item = {
+    id: id,
+    name: props.name,
+    image: props.image,
+    description: props.description,
+    price: props.price
+  }
 
   const handleAddToCart = () => {
-    dispatch(addToCart(props));
+    dispatch(addToCart(item));
   }
   return (
     <div>
